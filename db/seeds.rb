0a63c478e7aae50ed1f1f0eee303e739a7e71a45
Eduardo Serrano <eduardoserrano8591@gmail.com>
2021-11-10 15:16:50 -0500

    creates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Flat.create!(
+    name:Faker::FunnyName.two_word_name,
+    address:Faker::Address.street_address,
+    description:Faker::Lorem.sentence(word_count: 8),
+    price_per_night:Faker::Number.between(from: 75, to: 250),
+    number_of_guests:Faker::Number.between(from: 1, to: 7)
+  )
+end
